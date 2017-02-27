d610e3ddcaf22bbbf2e4392187b726db602e63dd
zachary-jones <zachary-jones@bisk.com>
2017-02-26 23:12:54 -0500

    added 2nd instapage test

--- routes/instapage/tests/tests.js
+++ routes/instapage/tests/tests.js
@@ -5,6 +5,10 @@ router.get('/instapage/', function(req, res, next) {
     res.render('instapage/tests/InstapageTest');
 });
 
+router.get('/instapage2/', function(req, res, next) {
+    res.render('instapage/tests/InstapageTest2');
+});
+
 router.get('/instapageTY/', function(req, res, next) {
     res.render('instapage/tests/InstapageTY');
 });
