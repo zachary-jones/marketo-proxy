0f8fc86743ca6f8986298f49130ee97818ca35c3
zachary-jones <zachary-jones@bisk.com>
2017-03-03 00:04:50 -0500

    added proxies

--- config/domainSFIDMap.js
+++ config/domainSFIDMap.js
@@ -4,7 +4,8 @@ var domainSFIDMap = [
     ['newenglandcollegeonline.com','0016100000TWYsYAAX'],
     ['villanovau.com','0016100000TWYsXAAX'],
     ['localhost','0016100000TWYsZAAX'],    
-    ['proxy','0016100000TWYsZAAX']    
+    ['bisk-marketo-proxy-staging.herokuapp.com','0016100000TWYsZAAX'],
+    ['bisk-marketo-proxy.herokuapp.com','0016100000TWYsZAAX']    
 ]
 
 module.exports = domainSFIDMap;
