9a3b48ba367cae0351a205b24c3321a7495efc6d
Zachary <joneszj@gmail.com>
2017-03-16 18:45:37 -0400

    began work on enrollment team help file

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,12 @@ router.get('/', function (req, res, next) {
     res.render("index", { api: req.app.get("api") });
 });
 
-router.get('/help', function (req, res, next) {
+router.get('/help - Marketing', function (req, res, next) {
     res.render("help", { api: req.app.get("api") });
+});
+
+router.get('/help - Enrollment Team', function (req, res, next) {
+    res.render("helpEnroll", { api: req.app.get("api") });
 });
 
 module.exports = router;
