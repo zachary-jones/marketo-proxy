540895fb72a2e111383e681c31dd226925b10579
Zachary <joneszj@gmail.com>
2017-03-20 14:46:06 -0400

    adding enrollment hlp section

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ router.get('/', function (req, res, next) {
     res.render("index", { api: req.app.get("api") });
 });
 
-router.get('/help - Marketing', function (req, res, next) {
+router.get('/helpMarketing', function (req, res, next) {
     res.render("help", { api: req.app.get("api") });
 });
 
-router.get('/help - Enrollment Team', function (req, res, next) {
+router.get('/helpEnrollmentTeam', function (req, res, next) {
     res.render("helpEnroll", { api: req.app.get("api") });
 });
 
