fc5761840c315de93ce4cf8519397535928ca93a
Zachary <joneszj@gmail.com>
2017-06-16 15:21:18 -0400

    code for heroku sendgrid addon...

--- repositories/features/mailer.js
+++ repositories/features/mailer.js
@@ -8,14 +8,43 @@ var transporter = nodemailer.createTransport({
     }
 });
 
+//sendGridHerokueAddon
+function sendGridHerokuAddon(mailer) {
+    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: mail.toJSON(),
+    });
+    sg.API(request, function(error, response) {
+        console.log(response.statusCode);
+        console.log(response.body);
+        console.log(response.headers);
+    });    
+}
+
+
 module.exports = {
     sendMessage : function(options) {
         console.log("Beginning email transmission process...");
-        try {
-            transporter.sendMail(options);
-        } catch (error) {
-            console.log("Error sending email...");
-            console.log(error);
+        console.log(options);
+        if (process.env.mode != 'local') {
+            this.sendGridHerokuAddon('test', 'test email');
+        } else {
+            try {
+                transporter.sendMail(options);
+            } catch (error) {
+                console.log("Error sending email...");
+                console.log(error);
+            }
         }
+    }, sendGridHerokuAddon: function(emailContent, emailSubject) {
+        var helper = require('sendgrid').mail;
+        var from_email = new helper.Email('marketo-proxy-leads@bisk.com');
+        var to_email = new helper.Email((process.env.mode == 'local' ? 'zachary-jones@bisk.com' : "Marketing-Developers@bisk.com"));
+        var subject = emailSubject;
+        var content = new helper.Content('text/plain', emailContent);
+        var mail = new helper.Mail(from_email, subject, to_email, content);        
+        sendGridHerokueAddon(mail);
     }
 }
