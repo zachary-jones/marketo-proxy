e5c9ce1875c055be25ddb3e21f403bae498a422e
Zachary <joneszj@gmail.com>
2017-06-29 10:57:56 -0400

    extended fields repo to get all fields

--- repositories/mkto/fields.js
+++ repositories/mkto/fields.js
@@ -9,12 +9,25 @@ function getFormFieldsByFormId(data, id, callback) {
     mktoHelper.makeRequest(requestObject, callback);
 }
 
+function getFormFields(data, callback) {
+    data.query = {
+        maxReturn: 200
+    }
+    var requestObject = mktoHelper.requestObject("/rest/asset/v1/form/fields.json?" + querystring.stringify(data.query), "GET", 'Bearer' + data.access_token);    
+    mktoHelper.makeRequest(requestObject, callback);
+}
+
 var mktofields = { 
     getFormFieldsByFormId: function(id, callback) {
         mktoHelper.access_token(function(data) {
             getFormFieldsByFormId(data, id, callback);
         });
-    }    
+    },
+    getFormFields: function(id, callback) {
+        mktoHelper.access_token(function(data) {
+            getFormFields(data, id, callback);
+        });
+    }   
 };
 
 module.exports = function() {
--- routes/mkto/fields.js
+++ routes/mkto/fields.js
@@ -5,13 +5,22 @@ var mktoFields = require('../../repositories/mkto/fields')();
 /**
  * Returns all form fields on a form by Form ID
  */
-router.get('/getFormFieldsByFormId/:id', function(req, res, next) {
-    if (!req.params.id || req.params.id === ':id') { res.send('id required'); } else {
-    mktoFields.getFormFieldsByFormId(req.params['id'], function(data) {
-        res.header("Content-Type",'application/json');
-        res.send(JSON.stringify(data,null,4));
+router.get('/getFormFieldsByFormId/:id', function (req, res, next) {
+    if (!req.params.id || req.params.id === ':id') {
+        res.send('id required');
+    } else {
+        mktoFields.getFormFieldsByFormId(req.params['id'], function (data) {
+            res.header("Content-Type", 'application/json');
+            res.send(JSON.stringify(data, null, 4));
+        });
+    }
+});
+
+router.get('/getFormFields', function (req, res, next) {
+    mktoFields.getFormFields(function (data) {
+        res.header("Content-Type", 'application/json');
+        res.send(JSON.stringify(data, null, 4));
     });
-}
 });
 
 module.exports = router;
