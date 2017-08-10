78c9defdf2cb0cafcf97ab36e8d1c0e7e26019d5
Flow Davis <florence-davis@bisk.com>
2017-08-10 15:21:44 -0400

    Do not call; Default Campaign Associations;

--- repositories/mulesoft/boas.js
+++ repositories/mulesoft/boas.js
@@ -7,8 +7,37 @@ function getStudentStatus(domainid, email, env, callback) {
     mulesoftHelper.makeRequest(mulesoftHelper.buildOptions(api), callback);
 }
 
+function getDefaultOrganicCampaignId(domain) {
+    if ('floridatechonline.com'.indexOf(domain) > -1) {
+      return '70161000001CrgD'
+    }
+    if ('jacksonvilleu.com'.indexOf(domain) > -1) {
+      return '70161000001CtDj'
+    }
+    if ('newenglandcollegeonline.com'.indexOf(domain) > -1) {
+      return '70161000001Csxh'
+    }
+    if ('villanovau.com'.indexOf(domain) > -1) {
+      return '70161000001CtDk'
+    }    
+    if ('usfhealthonline.com'.indexOf(domain) > -1) {
+      return '70161000001CtDo'
+    }  
+    if ('michiganstateuniversityonline.com'.indexOf(domain) > -1) {
+      return '70161000001CtDl'
+    }  
+    if ('uscranton.com'.indexOf(domain) > -1) {
+      return '70161000001CtDm'
+    }                
+    if (domain.indexOf('valpoonline.com') > -1) {
+      return '70161000001CtDn'
+    }         
+    return ""        
+  }
+
 var boas = { 
-    getStudentAccountStatus: getStudentStatus
+    getStudentAccountStatus: getStudentStatus,
+    getDefaultOrganicCampaignId: getDefaultOrganicCampaignId
 };
 
 module.exports = function() {
