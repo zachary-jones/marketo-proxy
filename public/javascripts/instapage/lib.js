edeca24662fbd56ea424ca959c69e394a0ab9982
zachary-jones <zachary-jones@bisk.com>
2017-08-19 14:56:00 -0400

    added munchkin to instapage script

--- public/javascripts/instapage/lib.js
+++ public/javascripts/instapage/lib.js
@@ -1,4 +1,27 @@
 var instapage = (function () {
+    // munchkin
+    (function() {
+        var didInit = false;
+        function initMunchkin() {
+          if(didInit === false) {
+            didInit = true;
+            Munchkin.init('058-NIT-467');
+          }
+        }
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = '//munchkin.marketo.net/munchkin.js';
+        s.onreadystatechange = function() {
+          if (this.readyState == 'complete' || this.readyState == 'loaded') {
+            initMunchkin();
+          }
+        };
+        s.onload = initMunchkin;
+        document.getElementsByTagName('head')[0].appendChild(s);
+      })();
+    // / munchkin
+        
     // lib vars
     var availableSteps = [];
     var init = {
